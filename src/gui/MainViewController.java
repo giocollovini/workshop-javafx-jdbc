c4fd5e0bc14d4c094659ef7507024144adca2fe5
Giovani Collovini <gcollovini@gmail.com>
2020-05-05 12:03:16 -0300

    Initializing Action as Parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -36,19 +37,22 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();	
+		});
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 	}
 
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
@@ -63,7 +67,11 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().clear();
 			
 			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());			
+			mainVBox.getChildren().addAll(newVBox.getChildren());		
+			
+			//Comando para ativar a função passada por paremetro
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 			
 		}
 		catch (IOException e) {
@@ -71,7 +79,7 @@ public class MainViewController implements Initializable {
 		}
 	}
 	
-	
+	/*
 	private synchronized void loadView2(String absoluteName) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -97,5 +105,6 @@ public class MainViewController implements Initializable {
 			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
+	*/
 		
 }
